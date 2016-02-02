0fa2abcae3348fdccd4ab1e928c1946faf8e011e
Philibert O <pollier@student.42.fr>
2016-02-02 21:07:03 +0100

    ds

--- proto/cycle_normal/cycle_normal.ino
+++ proto/cycle_normal/cycle_normal.ino
@@ -1,18 +1,21 @@
 int drain_pump = 13;                 // LED connected to digital pin 13
 int cycle_pump = 13;                 // LED connected to digital pin 13
 int heat = 13;                 // LED connected to digital pin 13
-int vanne = 13;                 // LED connected to digital pin 13
+int vanne = 34;                 // LED connected to digital pin 13
 int ledPin = 46;                 // LED connected to digital pin 13
 
 void setup()
 {
   pinMode(ledPin, OUTPUT);      // sets the digital pin as output
+  // pinMode(vanne, OUTPUT);      // sets the digital pin as output
 }
 
 void loop()
 {
-  digitalWrite(ledPin, HIGH);   // sets the LED on
-  delay(1000);                  // waits for a second
   digitalWrite(ledPin, LOW);    // sets the LED off
+  Serial.begin(9600);
+  Serial.write("LOW\n");
   delay(1000);                  // waits for a second
+  digitalWrite(ledPin, HIGH);    // sets the LED off
+  delay(1000);
 }
