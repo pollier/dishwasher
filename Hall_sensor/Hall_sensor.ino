1b4dedc9610d0ded9f8a3fbf14001390219ca7d0
Philibert O <pollier@student.42.fr>
2016-02-21 01:02:44 +0100

    daw

--- Hall_sensor/Hall_sensor.ino
+++ Hall_sensor/Hall_sensor.ino
@@ -1,12 +1,26 @@
-int const sensorPin = 12;
+#define		PIN_SENSOR	12
+#define		PIN_LED		8
 
 void setup()
 {
-  Serial.begin(9600);
-  pinMode(sensorPin,INPUT_PULLUP);
-  }
+	Serial.begin(9600);
+	pinMode(PIN_SENSOR,INPUT_PULLUP);
+	pinMode(13, OUTPUT);
+
+}
 
 void loop()
 {
-  Serial.println(digitalRead(sensorPin));
+	digitalWrite(13, HIGH);
+	if(digitalRead(PIN_SENSOR))
+	{
+		Serial.println("NULL");
+		digitalWrite(PIN_LED, LOW);
+	}
+	else
+	{
+		Serial.println("1");
+		digitalWrite(PIN_LED, HIGH);
+	}
+	delay(250);
 }
