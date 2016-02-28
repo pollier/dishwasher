14d0971c23847e52859feae438666dd815f805bb
Philibert O <pollier@student.42.fr>
2016-02-28 13:25:28 +0100

    advanc

--- nano/nano.h
+++ nano/nano.h
@@ -1,13 +1,13 @@
 #ifndef		NANO_H
 #define		NANO_H
 
-#define		PIN_LEVEL		3
-#define		PIN_HEAT		4
-#define		PIN_VANNE	   	5
-#define		PIN_PUMP_CYCLE	6
-#define		PIN_PUMP_PURGE	7
-#define		PIN_DISPENSER	6
-#define		PIN_PORTE	   	8
+#define		PIN_LEVEL		0
+#define		PIN_HEAT		5
+#define		PIN_VANNE	   	6
+#define		PIN_PUMP_CYCLE	7
+#define		PIN_PUMP_PURGE	8
+#define		PIN_THERMOSTAT	3
+#define		PIN_PORTE		2
 #define		PIN_ONEWIRE		9
 
 #endif
--- nano/nano.ino
+++ nano/nano.ino
@@ -3,31 +3,29 @@
 void setup()
 {
 	Serial.begin(9600);
-	pinMode(PIN_LEVEL, INPUT_PULLUP);
 	pinMode(PIN_HEAT, OUTPUT);
 	digitalWrite(PIN_HEAT, HIGH);
+
 	pinMode(PIN_VANNE, OUTPUT);
 	digitalWrite(PIN_VANNE, HIGH);
+
 	pinMode(PIN_PUMP_PURGE, OUTPUT);
 	digitalWrite(PIN_PUMP_PURGE, HIGH);
+
 	pinMode(PIN_PUMP_CYCLE, OUTPUT);
 	digitalWrite(PIN_PUMP_CYCLE, HIGH);
+
+	pinMode(PIN_THERMOSTAT, INPUT_PULLUP);
+	pinMode(PIN_PORTE, INPUT_PULLUP);
 }
 
 void loop()
 {
 	Serial.println("Debut du programme dans 3 secondes");
 	delay(3000);
+	
 	while(1)
 	{
-		Serial.print("Niveau d'eau :");
-		Serial.println(digitalRead(PIN_LEVEL));
-		digitalWrite(PIN_PUMP_CYCLE, LOW);
-		digitalWrite(PIN_HEAT, LOW);
-		delay(1800000);
-		digitalWrite(PIN_HEAT, HIGH);
-		digitalWrite(PIN_PUMP_PURGE, HIGH);
-		delay(5000);
 
 	}
 }
